98448e56e39392dc518e2e4eb68473d72a73934e
Delmondes <wellington.apk@gmail.com>
2025-05-11 23:28:20 -0300

    feat(config): adiciona configuração modelmapper

---
+++ backend/src/main/java/com/thomasgreg/backend/config/ModelMapperConfig.java
@@ -0,0 +1,20 @@
+package com.thomasgreg.backend.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        modelMapper.getConfiguration().setPreferNestedProperties(false);
+
+        return modelMapper;
+    }
+    
+}
